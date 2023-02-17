0fb19f91c45a35f1bcf2f680f8ad7ce7affa4e91
DavidPeinadoD <davidpeinadodiaz@gmail.com>
2023-02-17 20:14:22 +0100

    Ejercicio 6 y 7

---
+++ Ejercicio6.java
@@ -0,0 +1,7 @@
+public class Ejercicio6 {
+
+    /*
+    El algoritmo indicado sería una especie de juego para geografía
+     */
+
+}
---
+++ Ejercicio7.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Ejercicio7 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int opcion;
+        do {
+            System.out.println("-----------------------------------");
+            System.out.println("1 - Una cita de la ciudad del miedo");
+            System.out.println("2 - Una cita de James Bond");
+            System.out.println("3 - Una cita de la vida es un largo rio tranquilo");
+            System.out.println("4 - Una cita de Star Wars");
+            System.out.println("5 - Salir de esta magnífica aplicación");
+            opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    System.out.println("¿Qué es el miedo? El miedo es la imaginación de la muerte");
+                    break;
+                case 2:
+                    System.out.println("Bond. James Bond");
+                    break;
+                case 3:
+                    System.out.println("La vida es un largo rio tranquilo");
+                    break;
+                case 4:
+                    System.out.println("No hay nada más peligroso que un Jedi con miedo");
+                    break;
+                case 5:
+                    System.out.println("Gracias por usar esta magnífica aplicación");
+                    break;
+                default:
+                    System.out.println("Opción incorrecta");
+            }
+        } while (opcion != 5);
+    }
+
+
+}
